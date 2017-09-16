01452bc03beeca835b1555b0bf2638ad441fb0af
Antoine BAGNAUD <bagnaud.antoine@gmail.com>
2017-09-16 16:46:42 +0200

    Add parse args fn

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,22 @@ use clap::{App, Arg};
 const CHARS: &'static [u8] = b"0123456789abcdef";
 const CHARS_UPPERCASE: &'static [u8] = b"0123456789ABCDEF";
 
+fn parse_args() -> clap::ArgMatches<'static> {
+    App::new("Logo color extract")
+        .version("1.0")
+        .about("Exctact all colors in hexadecimal from a logo")
+        .author("Antoine Bagnaud <bagnaud.antoine@gmail.com>")
+        .arg(Arg::with_name("FILE")
+             .required(true)
+             .takes_value(true)
+             .index(1)
+             .help("Path to a image file"))
+        .arg(Arg::with_name("uppercase")
+             .short("u")
+             .long("uppercase")
+             .help("Return hexadecimal with uppercase letters #4ADE65"))
+        .get_matches()
+}
 
 fn in_vector(vec: &Vec<String>, find: &String) -> bool {
     for vec in vec.into_iter() {
@@ -37,22 +53,8 @@ fn pixel_2_hex(pix: &Rgb<u8>, uppercase: bool) -> String {
     }
 }
 
-
 fn main() {
-    let matches = App::new("Logo color extract")
-        .version("1.0")
-        .about("Exctact all colors in hexadecimal from a logo")
-        .author("Antoine Bagnaud <bagnaud.antoine@gmail.com>")
-        .arg(Arg::with_name("FILE")
-             .required(true)
-             .takes_value(true)
-             .index(1)
-             .help("Path to a image file"))
-        .arg(Arg::with_name("uppercase")
-             .short("u")
-             .long("uppercase")
-             .help("Return hexadecimal with uppercase letters #4ADE65"))
-        .get_matches();
+    let matches = parse_args();
 
     let path = matches.value_of("FILE").unwrap();
 
